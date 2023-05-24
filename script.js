801b165a5db9767e3bd835fdce785eb718531b0d
Chinwe Onumonu <kaforglo122@yahoo.com>
2023-05-25 00:17:25 +0100

    use dom method to declare variables

---
+++ script.js
@@ -0,0 +1,4 @@
+let first = document.getElementById('first-input');
+let second = document.getElementById('second-input');
+var result = document.getElementById('answer')
+let button = document.getElementById('btn');
