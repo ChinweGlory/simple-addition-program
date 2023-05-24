6d5c769918a596ae84f902fefe1bcdd1eb18d163
Chinwe Onumonu <kaforglo122@yahoo.com>
2023-05-25 00:22:52 +0100

    declare the function

--- script.js
+++ script.js
@@ -2,3 +2,10 @@ let first = document.getElementById('first-input');
 let second = document.getElementById('second-input');
 var result = document.getElementById('answer')
 let button = document.getElementById('btn');
+
+function addNum() {
+    let firstNumber = first.value;
+    let secondNumber = second.value;
+    let result = firstNumber+secondNumber;
+    return result;
+}
