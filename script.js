dec3a1d78515913a23754afd048a38288332ed00
Chinwe Onumonu <kaforglo122@yahoo.com>
2023-05-25 09:33:24 +0100

    add eventListener

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-function addNum {
-let first =parseInt(document.getElementById("first-input").value);
+function addNum() {
+let first = parseInt(document.getElementById("first-input").value);
 let second= parseInt(document.getElementById("second-input").value);
 const result = document.getElementById("answer");
 answer.value = first + second;
